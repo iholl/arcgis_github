cf7546580f234d23d12cecb62aa9a4079ff305a7
iholl <ian.michael.holl@gmail.com>
2021-09-20 15:32:46 -0700

    Changed: time to date

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import os
 import arcgis
-from datetime import datetime
+from datetime import date
 from decouple import config
 
 # set secrets from github repo
-ARCGIS_ONLINE_USERNAME = os.environ['ARCGIS_ONLINE_USERNAME']
-ARCGIS_ONLINE_PASSWORD = os.environ['ARCGIS_ONLINE_PASSWORD']
+# ARCGIS_ONLINE_USERNAME = os.environ['ARCGIS_ONLINE_USERNAME']
+# ARCGIS_ONLINE_PASSWORD = os.environ['ARCGIS_ONLINE_PASSWORD']
 
 # set local env variables
-# ARCGIS_ONLINE_USERNAME = config('ARCGIS_ONLINE_USERNAME')
-# ARCGIS_ONLINE_PASSWORD = config('ARCGIS_ONLINE_PASSWORD')
+ARCGIS_ONLINE_USERNAME = config('ARCGIS_ONLINE_USERNAME')
+ARCGIS_ONLINE_PASSWORD = config('ARCGIS_ONLINE_PASSWORD')
 
-# get access to the fishable waters data and export the data to a GeoJson file in the arcgis online account
+# get access to the arcgis online content
 gis = arcgis.GIS(url=None, username=ARCGIS_ONLINE_USERNAME, password=ARCGIS_ONLINE_PASSWORD)
 
+# arcgis online feature layers
 fishable_lines = "69327cd8775f4683aecc5cfd71bf3b77"
 fishable_polygons = "8ec9b5ac269d40dca78af47a22e05af3"
 data_list = [fishable_lines, fishable_polygons]
 
-now = datetime.now()
-current_time = now.strftime("%H_%M_%S")
+# set current date
+today = date.today()
+current_time = today.strftime("%m_%d_%Y")
 
 for feature_layer in data_list:
+	# get feature layer by id and export to a GeoJson
 	data = gis.content.get(feature_layer)
 	data_title = data.title + "_" + current_time
 	export_geojson = data.export(title=data_title, export_format="GeoJson", parameters=None, wait=True)
