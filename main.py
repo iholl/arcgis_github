d747a3b841afd2c1ad6f1a8bee576fc838af06dd
iholl <ian.michael.holl@gmail.com>
2021-09-20 15:06:18 -0700

    Added: local environmental variables

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import os
 import arcgis
 from datetime import datetime
+from decouple import config
 
 # set secrets from github repo
 ARCGIS_ONLINE_USERNAME = os.environ['ARCGIS_ONLINE_USERNAME']
 ARCGIS_ONLINE_PASSWORD = os.environ['ARCGIS_ONLINE_PASSWORD']
 
+# set local env variables
+# ARCGIS_ONLINE_USERNAME = config('ARCGIS_ONLINE_USERNAME')
+# ARCGIS_ONLINE_PASSWORD = config('ARCGIS_ONLINE_PASSWORD')
+
 # get access to the fishable waters data and export the data to a GeoJson file in the arcgis online account
 gis = arcgis.GIS(url=None, username=ARCGIS_ONLINE_USERNAME, password=ARCGIS_ONLINE_PASSWORD)
 data = gis.content.get("69327cd8775f4683aecc5cfd71bf3b77")
 now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
+current_time = now.strftime("%H_%M_%S")
 data_title = data.title + "_" + current_time
 export_geojson = data.export(title=data_title, export_format="GeoJson", parameters=None, wait=True)
 print("export geojson created in arcgis online account")
@@ -19,6 +24,8 @@ print("export geojson created in arcgis online account")
 export_geojson.download(save_path="fishable_waters_lines")
 export_geojson.delete()
 print('downloaded fishable_waters_lines')
+
+
 
 
 
