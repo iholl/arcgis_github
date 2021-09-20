1e543d5fb7a5ecc7bbc38281ceeaedb8687b943a
iholl <ian.michael.holl@gmail.com>
2021-09-20 12:30:01 -0700

    Removed: datetime in file name

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import arcgis
-from datetime import datetime
 
 # get access to the fishable waters data and export the data to a GeoJson file in the arcgis online account
 gis = arcgis.GIS(url=None, username='NDOW_DEV', password='nd0wM@PS1!')
 data = gis.content.get('69327cd8775f4683aecc5cfd71bf3b77')
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-data_title = data.title + '_' + current_time
+data_title = data.title
 export_geojson = data.export(title=data_title, export_format='GeoJson', parameters=None, wait=True)
 print('export geojson created in arcgis online account')
 
